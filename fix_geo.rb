d947cb49d360ed1029b24022534362ae00b59a98
dl-maura <maura_carbone@harvard.edu>
2021-05-18 10:42:56 -0400

    License and Array Fix
    Add license statement and make array uniq

--- fix_geo.rb
+++ fix_geo.rb
@@ -1,3 +1,16 @@
+#   Copyright @2021 President and Fellows of Harvard College
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 require 'json'
 
 def fix_bbox(bbox_string, layer, bad_file)
@@ -62,7 +75,8 @@ for file in file_list
             File.open(file, 'w') { |f| f.write(json_file.to_json) }
         end
     end
-    changes = File.open('/path/to/files/fixed.txt', 'a') 
+    changes = File.open('/path/to/files/fixed.txt', 'a')
+    bad_stuff=bad_stuff.uniq
     for item in bad_stuff
         changes.write(item.to_s + "\n")
     end
